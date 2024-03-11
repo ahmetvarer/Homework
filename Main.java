9a56582c9d22e2895546d2bb6eab7be5802e33ed
ahmetvarer <163045895+ahmetvarer@users.noreply.github.com>
2024-03-11 23:42:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Ogrenci ogrenci = new Ogrenci("ahmet", "bilgisayar müh", 3.2, 12, 141, 2023);
+		ogrenci.bilgileriYazdir();
+
+	}
+}
